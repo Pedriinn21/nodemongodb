4be1a13d2b76b51be6c0fdc9d7fce1119c9a3317
Pedriinn21 <pedro.silva10@iesb.edu.br>
2024-03-22 21:33:10 -0300

    nodemongodb

--- routes/routes.js
+++ routes/routes.js
@@ -10,10 +10,16 @@ router.get('/produtos', async function(req, res){
 
 })
 router.post('/produtos', async function(req, res){
-    res.json( await Produto.create({nome: 'Pizza'}) )
+    res.json( await Produto.create(req.body) )
 })
 router.get('/produtos/:id', async function(req, res){
-    res.json( await Produto.findById() )
+    res.json( await Produto.findById(req.params.id) )
+})
+router.delete('/produtos/:id', async function(req, res){
+    res.json( await Produto.findByIdAndDelete(req.params.id) )
+})
+router.put('/produtos/:id', async function(req, res){
+    res.json( await Produto.findByIdAndUpdate(req.params.id, req.body) )
 })
 
 
